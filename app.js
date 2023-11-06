5e1b0a75621c3050b4e626b840fedccfbf87c331
Shahmeer09 <shahmeerrizwan921@gmail.com>
2023-11-06 16:17:46 +0500

    LOOPS P-2 JS

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+//question 7
+
+// document.write(`<h2>Counting: </h2>`)
+// for (i = 1; i <= 15; i++){
+//     document.write(`${[i]}, `)
+// }
+
+// document.write('<h2>Reverse Counting: </h2>')
+// for (let i = 10; i >= 1; i--){
+//     document.write(`${[i]}, `)
+// }
+
+// document.write('<h2>Even: </h2>')
+// for (let i = 0; i <= 20; i += 2){
+//     document.write(`${[i]}, `)
+// }
+
+// document.write('<h2>Odd: </h2>')
+// for (let i = 1; i <= 19; i += 2){
+//     document.write(`${[i]}, `)
+// }
+
+// document.write('<h2>Series: </h2>')
+// for (let i = 2; i <= 20; i += 2){
+//     document.write(`${[i]}k, `)
+// }
+
+
+
+
+
+//question 9
+
+// let A = [24, 53, 78, 91, 12]
+// let largestNum = A[0]
+
+// for (let i = 1; i < A.length; i++){
+//     if (A[i] > largestNum){
+//         largestNum = A[i]
+//     }
+// }
+// console.log(`The Largest number in the Array is ${[largestNum]}`);
+
+
+//question 10
+
+// let A = [24, 53, 78, 91, 12]
+// let smallestNum = A[0]
+
+// for (let i = 1; i < A.length; i++){
+//     if (A[i] < smallestNum){
+//         smallestNum = A[i]
+//     }
+// }
+// console.log(`The smallest number in the Array is ${[smallestNum]}`);
